a4fa247898317ddf539f1b0a29f5940c4c9323a3
Stéphane Menut <smenut@dawan.fr>
2021-01-08 13:47:20 +0100

    authentication

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -10,16 +11,24 @@ export class AuthenticationComponent implements OnInit {
 
   state = false;
 
-  constructor(private auth: AuthenticationService) { }
+  constructor(
+    private auth: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-
     this.state = this.auth.isAuthenticate();
-
   }
 
   public logIn(): void {
     this.state = this.auth.login();
+
+    // return (cond) ? true: false;
+
+    if ( this.route.snapshot.queryParamMap.has('redirect') ) {
+      this.router.navigateByUrl( this.route.snapshot.queryParamMap.get('redirect') );
+    }
   }
 
   public logOut(): void {
--- src/app/guards/secureRoute/secure-route.guard.ts
+++ src/app/guards/secureRoute/secure-route.guard.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecureRouteGuard implements CanActivate {
 
+  constructor(
+    private authentication: AuthenticationService,
+    private router: Router
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
+    if ( this.authentication.isAuthenticate() ){
+      return this.authentication.isAuthenticate();
+    }
 
-    return true;
+    return this.router.parseUrl(`/authentication?redirect=${state.url}`);
   }
 
 }
